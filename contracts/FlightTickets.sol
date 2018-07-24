3c8eb9dbfd7e42510956d93a221cf23cb2482a2f
Roman V <dev.romanv@gmail.com>
2018-07-25 00:20:36 +0700

    Added Tickets logic to the contract. Added tests for that logic.

--- contracts/FlightTickets.sol
+++ contracts/FlightTickets.sol
@@ -8,13 +8,34 @@ import "../installed_contracts/zeppelin/contracts/ownership/Ownable.sol";
  * @author Roman Vinogradov <dev.romanv@gmail.com>
  */
 contract FlightTickets is Ownable {
+  // Library that allows overflow-safe arithmetic operations
+  // Used like this: a.add(b) or a.mul(b) where a & b are uint256
   using SafeMath for uint256;
 
-  // Airline has an auto-incremented ID, a unique name and an owner
   struct Airline {
+    // Unique autoincremented ID
     uint256 aId;
+    // Unique airline name
     bytes32 aName;
+    // Ethereum address of the owner who will manage this airline
     address aOwner;
+  }
+
+  struct Ticket {
+    // Unique autoincremented ID
+    uint256 tId;
+    // ID of the airline that provides this ticket
+    uint256 aId;
+    // From and To, cities connected by the flight
+    bytes32 tFrom;
+    bytes32 tTo;
+    // Ticket price in wei
+    uint256 tPrice;
+    // Number of seats available at this price
+    uint256 tQuantity;
+    // Timestamps of planned departure and arrival
+    uint256 tDeparture;
+    uint256 tArrival;
   }
 
   // Auxiliary entity to keep a reference to a particular entry in an array
@@ -23,15 +44,57 @@ contract FlightTickets is Ownable {
     uint256 index;
   }
 
+  // Storage of airlines
   Airline[] public airlines;  // The list of airlines
   uint256 private aIdLast;  // Last airline ID generated, used for autoincrementing
   mapping(uint256 => ArrayIndex) private airlineIdIndex;  // Index to find Airline by its ID
-  mapping(bytes32 => bool) private airlineNameExists;  // To keep track of which names are taken
+  mapping(bytes32 => bool) private airlineNameExists;  // To keep track of which airline names are taken
 
-  // These are triggered when a particular airline in the list is added, changed or deleted
+  // Storage of tickets
+  Ticket[] public tickets;  // The list of tickets
+  uint256 private tIdLast;  // Last ticket ID generated, used for autoincrementing
+  mapping(uint256 => ArrayIndex) private ticketIdIndex;  // Index to find Ticket by its ID
+  mapping(uint256 => uint256[]) private ticketsByAirline;  // To find list of Ticket IDs by their Airline ID
+  // The following is a two-dimensional index to find a certain entry in an array ticketsByAirline[aId]
+  mapping(uint256 => mapping(uint256 => ArrayIndex)) private ticketsByAirlineIndex;
+
+  // When an airline in the list is added, changed or deleted
   event LogAirlineAdded(uint256 indexed aId, bytes32 indexed aName, address aOwner);
   event LogAirlineUpdated(uint256 indexed aId, bytes32 newAName, address newAOwner);
   event LogAirlineRemoved(uint256 indexed aId);
+  // When a ticket is added, changed or deleted
+  event LogTicketAdded(
+    uint256 indexed tId, uint256 aId, bytes32 tFrom, bytes32 tTo, uint256 tPrice,
+    uint256 tQuantity, uint256 tDeparture, uint256 tArrival
+  );
+  event LogTicketUpdated(uint256 indexed tId, uint256 newTPrice, uint256 newTQuantity);
+  event LogTicketRemoved(uint256 indexed tId);
+
+  /**
+   * @dev Make sure the caller is the owner of the airline
+   * @param _aId The airline ID
+   */
+  modifier onlyAirlineOwner(uint256 _aId) {
+    require(airlineIdIndex[_aId].exists, "Airline does not exist");
+    require(airlines[airlineIdIndex[_aId].index].aOwner == msg.sender, "Not the airline owner");
+    _;
+  }
+
+  /**
+   * @dev Make sure the caller is the owner of the ticket
+   * @param _tId The ticket ID
+   */
+  modifier onlyTicketOwner(uint256 _tId) {
+    // find the ticket
+    require(ticketIdIndex[_tId].exists, "Ticket does not exist");
+    Ticket memory ticket = tickets[ticketIdIndex[_tId].index];
+    // find the airline
+    require(airlineIdIndex[ticket.aId].exists, "Airline does not exist anymore");
+    Airline memory airline = airlines[airlineIdIndex[ticket.aId].index];
+    // make sure the caller is the owner of the airline the ticket belongs to
+    require(airline.aOwner == msg.sender, "Not the ticket owner");
+    _;
+  }
 
   /**
    * @notice Check if the airline name is taken or not
@@ -59,7 +122,7 @@ contract FlightTickets is Ownable {
    * @param _aOwner Address of the airline owner, can be any Ethereum address
    */
   function addAirline(bytes32 _aName, address _aOwner) public onlyOwner {
-    require(!airlineExists(_aName));
+    require(!airlineExists(_aName), "Airline name is already taken");
     // generate new airline ID
     uint256 _aId = aIdLast.add(1);
     aIdLast = _aId;
@@ -79,12 +142,12 @@ contract FlightTickets is Ownable {
    * @param _newAOwner New owner of the airline
    */
   function editAirline(uint256 _aId, bytes32 _newAName, address _newAOwner) public onlyOwner {
-    require(airlineIdIndex[_aId].exists);
+    require(airlineIdIndex[_aId].exists, "Airline does not exist");
     // get index of the array element being changed
     uint256 _index = airlineIdIndex[_aId].index;
     // if the name has changed, check it's unique and save it
     if (_newAName != airlines[_index].aName) {
-      require(!airlineExists(_newAName));
+      require(!airlineExists(_newAName), "New airline name is already taken");
       // free the old name, occupy the new one
       airlineNameExists[airlines[_index].aName] = false;
       airlineNameExists[_newAName] = true;
@@ -102,7 +165,7 @@ contract FlightTickets is Ownable {
    * @param _aId ID of the airline to remove
    */
   function removeAirline(uint256 _aId) public onlyOwner {
-    require(airlineIdIndex[_aId].exists);
+    require(airlineIdIndex[_aId].exists, "Airline does not exist");
     // get index of the array element being removed
     uint256 _index = airlineIdIndex[_aId].index;
     // remove the airline and free its name
@@ -119,6 +182,110 @@ contract FlightTickets is Ownable {
     // remove the last element of the array
     airlines.length--;
     emit LogAirlineRemoved(_aId);
+  }
+
+  /**
+   * @notice Get the number of tickets from a given airline
+   * @dev Use it in the frontend to iterate over tickets by airline
+   * @param _aId ID of the airline
+   * @return The number of tickets owned by the airline
+   */
+  function getTicketsCount(uint256 _aId) public view returns (uint256) {
+    require(airlineIdIndex[_aId].exists, "Airline does not exist");
+    return ticketsByAirline[_aId].length;
+  }
+
+  /**
+   * @notice Add an ticket to the list
+   * @dev Use the emitted LogTicketAdded event to get the new ticket's generated ID
+   * @param _aId ID of the airline; the airline must be owned by the caller!
+   * @param _tFrom City from which the flight departs
+   * @param _tTo City to which the flight arrives
+   * @param _tPrice Price for the ticket in wei
+   * @param _tQuantity Number of seats available for this flight at this price
+   * @param _tDeparture Timestamp of the planned departure
+   * @param _tArrival Timestamp of the planned arrival
+   */
+  function addTicket(
+    uint256 _aId, bytes32 _tFrom, bytes32 _tTo, uint256 _tPrice,
+    uint256 _tQuantity, uint256 _tDeparture, uint256 _tArrival
+  ) public onlyAirlineOwner(_aId) {
+    // generate new ticket ID
+    uint256 _tId = tIdLast.add(1);
+    tIdLast = _tId;
+    // add a new Ticket record to tickets array and save its index in the array
+    Ticket memory ticket = Ticket(
+      _tId, _aId, _tFrom, _tTo, _tPrice, _tQuantity, _tDeparture, _tArrival
+    );
+    uint256 _index = tickets.push(ticket) - 1;
+    ticketIdIndex[_tId].exists = true;
+    ticketIdIndex[_tId].index = _index;
+    // add the ticket ID to the list of this airline's tickets
+    uint256 _index2 = ticketsByAirline[_aId].push(_tId) - 1;
+    // and save the index of this entry too, to be able to manage it later
+    ticketsByAirlineIndex[_aId][_tId].exists = true;
+    ticketsByAirlineIndex[_aId][_tId].index = _index2;
+    emit LogTicketAdded(
+      _tId, _aId, _tFrom, _tTo, _tPrice, _tQuantity, _tDeparture, _tArrival
+    );
+  }
+
+  /**
+   * @notice Change an existing ticket
+   * @param _tId ID of the ticket that is being changed (ID itself does not change)
+   * @param _newTPrice New price of the ticket
+   * @param _newTQuantity New number of seats available
+   */
+  function editTicket(uint256 _tId, uint256 _newTPrice, uint256 _newTQuantity) public onlyTicketOwner(_tId) {
+    // update the ticket data
+    uint256 _index = ticketIdIndex[_tId].index;
+    tickets[_index].tPrice = _newTPrice;
+    tickets[_index].tQuantity = _newTQuantity;
+    emit LogTicketUpdated(_tId, _newTPrice, _newTQuantity);
+  }
+
+  /**
+   * @notice Remove a ticket from the list
+   * @dev Removal operation changes the order of some items in tickets array, so don't rely
+   * on the order of the array, sort it yourself when you need it. It also changes the order
+   * of some items in the ticketsByAirline[aId] array, same thing.
+   *
+   * PS: You'll likely break your brain trying to grasp what's going on here,
+   * but I've done my best to make the code as understandable as possible.
+   * Thanks to the ease of maintaining complex data structures in Solidity :D
+   *
+   * @param _tId ID of the ticket to remove
+   */
+  function removeTicket(uint256 _tId) public onlyTicketOwner(_tId) {
+    // save the ticket's airline ID for the later use
+    uint256 _aId = tickets[_index].aId;
+    // remove the ticket
+    ticketIdIndex[_tId].exists = false;
+    if (tickets.length > 1) {
+      // get index of the array element being removed
+      uint256 _index = ticketIdIndex[_tId].index;
+      // the same trick is used as in removeAirline():
+      // move the last element of the array in place of the removed one
+      tickets[_index] = tickets[tickets.length-1];
+      // update the ID index
+      ticketIdIndex[tickets[_index].tId].index = _index;
+    }
+    // remove ticket ID from the list of airline's tickets
+    ticketsByAirlineIndex[_aId][_tId].exists = false;
+    if (ticketsByAirline[_aId].length > 1) {
+      // same trick but the indexes are more complex (two-dimensional actually)
+      uint256 _index2 = ticketsByAirlineIndex[_aId][_tId].index;
+      // move last element of ticketsByAirline[_aId] array in place of the removed one
+      ticketsByAirline[_aId][_index2] = ticketsByAirline[_aId][ticketsByAirline[_aId].length-1];
+      // and update the relevant index
+      uint256 movedTicketId = ticketsByAirline[_aId][_index2];
+      ticketsByAirlineIndex[_aId][movedTicketId].index = _index2;
+    }
+    // remove the last element
+    ticketsByAirline[_aId].length--;
+    // remove the last element of the array
+    tickets.length--;
+    emit LogTicketRemoved(_tId);
   }
 
 }
--- test/TestFlightTickets.js
+++ test/TestFlightTickets.js
@@ -16,22 +16,22 @@ contract('FlightTickets', accounts => {
     flightTickets = await FlightTickets.deployed();
   });
 
-  it('sets owner', async () => {
+  it('sets the owner', async () => {
     assert.equal(await flightTickets.owner.call(), accounts[0]);
   });
 
-  it('does not allow to add an airline from non-owner', async () => {
+  it('does not allow to add an airline from a non-owner', async () => {
     assert.ok(await hasReverted(
       flightTickets.addAirline('Test Airline', accounts[2], { from: accounts[1] })
     ));
   });
 
-  it('adds an airline from owner', async () => {
+  it('adds an airline from the owner', async () => {
     await flightTickets.addAirline('Test Airline', accounts[2], { from: accounts[0] });
     assert.equal(await flightTickets.getAirlinesCount.call(), 1);
   });
 
-  it('confirms that airline exists', async () => {
+  it('confirms that the airline exists', async () => {
     let exists = await flightTickets.airlineExists.call('Test Airline');
     assert.ok(exists);
   });
@@ -70,9 +70,67 @@ contract('FlightTickets', accounts => {
     let count = await flightTickets.getAirlinesCount.call();
     await flightTickets.removeAirline(1);
     let newCount = await flightTickets.getAirlinesCount.call();
-    assert.equal(newCount, count-1);
+    assert.equal(newCount, count - 1);
     let exists = await flightTickets.airlineExists('New Airline Name');
     assert.ok(!exists);
+  });
+
+  // existing airline by now: {aId: 2, aName: 'Second Airline', aOwner: accounts[4]}
+  // we will use it for the ticket-related tests below
+
+  const AID = 2;
+  const AOWNER = accounts[4];
+  const TPRICE = web3.toWei(100, 'finney');
+  const TFROM = 'Hong Kong';
+  const TTO = 'Denpasar';
+  const TQUANTITY = 150;
+  const TDEPARTURE = 1536573600;
+  const TARRIVAL = 1536589800;
+
+  it('does not allow to add a ticket from a non-owner of the airline', async () => {
+    assert.ok(await hasReverted(
+      flightTickets.addTicket(AID, TFROM, TTO, TPRICE, TQUANTITY, TDEPARTURE, TARRIVAL, { from: accounts[0] })
+    ));
+  });
+
+  it('adds tickets from the owner of the airline', async () => {
+    await flightTickets.addTicket(AID, TFROM, TTO, TPRICE, TQUANTITY, TDEPARTURE, TARRIVAL, { from: AOWNER });
+    // add some more
+    await flightTickets.addTicket(AID, 'Denpasar', 'Tokyo', web3.toWei(300, 'finney'), 50, 1536589800, 1536607800, { from: AOWNER });
+    await flightTickets.addTicket(AID, 'Zhengzhou', 'Singapore', web3.toWei(50, 'finney'), 200, 1536589800, 1536607800, { from: AOWNER });
+    assert.equal(await flightTickets.getTicketsCount.call(AID), 3);
+  });
+
+  const TID = 1;
+
+  it('stores the ticket data', async () => {
+    let [tId, aId, tFrom, tTo, tPrice, tQuantity, tDeparture, tArrival] = await flightTickets.tickets.call(0);
+    tFrom = web3.toUtf8(tFrom);
+    tTo = web3.toUtf8(tTo);
+    assert.equal(tId, TID);
+    assert.equal(aId, AID);
+    assert.equal(tFrom, TFROM);
+    assert.equal(tTo, TTO);
+    assert.equal(tPrice, TPRICE);
+    assert.equal(tQuantity, TQUANTITY);
+    assert.equal(tDeparture, TDEPARTURE);
+    assert.equal(tArrival, TARRIVAL);
+  });
+
+  it('edits a ticket', async () => {
+    let newPrice = TPRICE * 2;
+    let newQuantity = TQUANTITY - 50;
+    await flightTickets.editTicket(TID, newPrice, newQuantity, { from: AOWNER });
+    let [tId, aId, tFrom, tTo, tPrice, tQuantity, tDeparture, tArrival] = await flightTickets.tickets.call(0);
+    assert.equal(tPrice, newPrice);
+    assert.equal(tQuantity, newQuantity);
+  });
+
+  it('removes a ticket', async () => {
+    let count = await flightTickets.getTicketsCount.call(AID);
+    await flightTickets.removeTicket(TID, { from: AOWNER });
+    let newCount = await flightTickets.getTicketsCount.call(AID);
+    assert.equal(newCount, count - 1);
   });
 
 });

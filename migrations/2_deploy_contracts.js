129187d87612f2a66bfcfd979f3fafb8eee06eec
Roman V <dev.romanv@gmail.com>
2018-08-04 13:25:50 +0800

    Removed unnecessary actions from migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,8 @@
 var SafeMath = artifacts.require('../installed_contracts/zeppelin/contracts/math/SafeMath.sol');
-var Ownable = artifacts.require('../installed_contracts/zeppelin/contracts/ownership/Ownable.sol');
 var FlightTickets = artifacts.require("./FlightTickets.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
   deployer.link(SafeMath, FlightTickets);
-  deployer.deploy(Ownable);
-  deployer.link(Ownable, FlightTickets);
   deployer.deploy(FlightTickets);
 };

60a2b0d1f9bc48b190dae6b6f5ff81f0e84d42ae
Roman V <dev.romanv@gmail.com>
2018-08-06 12:27:02 +0800

    Improved the tests structure, added thorough comments.

--- test/TestFlightTickets.js
+++ test/TestFlightTickets.js
@@ -1,5 +1,6 @@
 const FlightTickets = artifacts.require("FlightTickets");
 
+// Function to verify that a contract call has failed (reverted) during execution
 async function hasReverted(contractCall) {
   try {
     await contractCall;
@@ -9,45 +10,55 @@ async function hasReverted(contractCall) {
   }
 }
 
+// Convenience function, to use human-readable dates in the code and still send
+// unix timestamps to the contract
 function toUTCTimestamp(datestr) {
   return Date.parse(datestr + '+00:00') / 1000;
 }
 
+// Sample airline logo that we'll use in the tests
 const aLogoHash = 'QmZ9Nbn5Bfcf28p5Mn9Aobw2hvkW4ANxJJDBZdh5kUyQPm';
 
 contract('FlightTickets', accounts => {
   let flightTickets;
 
-  beforeEach(async () => {
-    flightTickets = await FlightTickets.deployed();
+  // Deploy the contract
+  before(async () => {
+    flightTickets = await FlightTickets.new();
   });
 
+  // Check that contract ownership is set properly
   it('sets the owner', async () => {
     assert.equal(await flightTickets.owner.call(), accounts[0]);
   });
 
+  // Check that only the owner can add an airline
   it('does not allow to add an airline from a non-owner', async () => {
     assert.ok(await hasReverted(
       flightTickets.addAirline('Test Airline', accounts[2], aLogoHash, { from: accounts[1] })
     ));
   });
 
+  // Check that an airline is added when the owner is doing it
   it('adds an airline from the owner', async () => {
     await flightTickets.addAirline('Test Airline', accounts[2], aLogoHash, { from: accounts[0] });
     assert.equal(await flightTickets.getAirlinesCount.call(), 1);
   });
 
+  // Check that the airline name is now taken
   it('confirms that the airline exists', async () => {
     let exists = await flightTickets.airlineExists.call('Test Airline');
     assert.ok(exists);
   });
 
+  // Check that it's not possible to add an airline with a non-unique name
   it('does not allow to add an airline when the name is taken', async () => {
     assert.ok(await hasReverted(
       flightTickets.addAirline('Test Airline', accounts[2], aLogoHash, { from: accounts[0] })
     ));
   });
 
+  // Check that all airline details are saved correctly
   it('stores the airline data', async () => {
     let [aId, aName, aOwner, aLogo] = await flightTickets.airlines.call(0);
     aName = web3.toUtf8(aName);
@@ -57,6 +68,7 @@ contract('FlightTickets', accounts => {
     assert.equal(aLogo, aLogoHash);
   });
 
+  // Check that an airline can be edited and new details are saved correctly
   it('edits an airline', async () => {
     let aNewLogoHash = 'QmP8AdSRBQeMNwt6kyufDDssrA6RxrPB74LEoJgMpYTa6S';
     await flightTickets.editAirline(1, 'New Airline Name', accounts[3], aNewLogoHash, { from: accounts[0] });
@@ -68,6 +80,7 @@ contract('FlightTickets', accounts => {
     assert.equal(aLogo, aNewLogoHash);
   });
 
+  // Check that name uniqueness validation works with editing as well
   it('does not allow to edit an airline when the new name is taken', async () => {
     await flightTickets.addAirline('Second Airline', accounts[4], aLogoHash, { from: accounts[0] });
     assert.ok(await hasReverted(
@@ -75,6 +88,7 @@ contract('FlightTickets', accounts => {
     ));
   });
 
+  // Check that airline is removed correctly and its name is then freed
   it('removes an airline', async () => {
     let count = await flightTickets.getAirlinesCount.call();
     await flightTickets.removeAirline(1, { from: accounts[0] });
@@ -84,9 +98,10 @@ contract('FlightTickets', accounts => {
     assert.ok(!exists);
   });
 
-  // existing airline by now: {aId: 2, aName: 'Second Airline', aOwner: accounts[4]}
-  // we will use it for the ticket-related tests below
+  // Existing airline by now: {aId: 2, aName: 'Second Airline', aOwner: accounts[4]}
+  // We will use it for the ticket-related tests below
 
+  // Set up some sample ticket data to reuse in the tests below
   const AID = 2;
   const AOWNER = accounts[4];
   const TPRICE = web3.toWei(100, 'finney');
@@ -96,12 +111,14 @@ contract('FlightTickets', accounts => {
   const TDEPARTURE = 1536573600;
   const TARRIVAL = 1536589800;
 
+  // Check that airline ownership is validated correctly when adding adding tickets
   it('does not allow to add a ticket from a non-owner of the airline', async () => {
     assert.ok(await hasReverted(
       flightTickets.addTicket(AID, TFROM, TTO, TPRICE, TQUANTITY, TDEPARTURE, TARRIVAL, { from: accounts[0] })
     ));
   });
 
+  // Check that it adds some tickets successfully if the airline owner is doing it
   it('adds tickets from the owner of the airline', async () => {
     await flightTickets.addTicket(AID, TFROM, TTO, TPRICE, TQUANTITY, TDEPARTURE, TARRIVAL, { from: AOWNER });
     // add some more
@@ -110,8 +127,10 @@ contract('FlightTickets', accounts => {
     assert.equal(await flightTickets.getTicketsCount.call(AID), 3);
   });
 
+  // ID of an existing ticket that we're going to reuse below
   const TID = 1;
 
+  // Check that all ticket data is saved correctly
   it('stores the ticket data', async () => {
     let [tId, aId, tFrom, tTo, tPrice, tQuantity, tDeparture, tArrival] = await flightTickets.tickets.call(0);
     tFrom = web3.toUtf8(tFrom);
@@ -126,6 +145,7 @@ contract('FlightTickets', accounts => {
     assert.equal(tArrival, TARRIVAL);
   });
 
+  // Check that a ticket can be edited and the ticket data is updated correctly
   it('edits a ticket', async () => {
     let newPrice = TPRICE * 2;
     let newQuantity = TQUANTITY - 50;
@@ -135,6 +155,7 @@ contract('FlightTickets', accounts => {
     assert.equal(tQuantity, newQuantity);
   });
 
+  // Check that a ticket is removed correctly
   it('removes a ticket', async () => {
     let count = await flightTickets.getTicketsCount.call(AID);
     await flightTickets.removeTicket(TID, { from: AOWNER });
@@ -142,6 +163,7 @@ contract('FlightTickets', accounts => {
     assert.equal(newCount, count - 1);
   });
 
+  // Check that direct a flight is found correctly by "from", "to" and "when"
   it('finds a direct flight', async () => {
     await flightTickets.addTicket(
       AID, 'Bangkok', 'Dubai', web3.toWei(150, 'finney'), 50,
@@ -169,6 +191,7 @@ contract('FlightTickets', accounts => {
     assert.equal(tTo, 'London');
   });
 
+  // Check that a two-tickets flight is found correctly, sequencing the tickets above
   it('finds a one-stop flight', async () => {
     let when = Date.parse('2018-12-10') / 1000;
     flights = await flightTickets.findOneStopFlights.call('Bangkok', 'London', when);
@@ -187,6 +210,8 @@ contract('FlightTickets', accounts => {
     assert.equal(tTo1, tFrom2);
   });
 
+  // Check that a direct flight is bought correctly, the airline receives the payment
+  // and the buyer receives the change
   it('books a direct flight', async () => {
     let [tId, aId, , , tPrice, tQuantity, ,] = await flightTickets.tickets.call(0);
     let [, , aOwner,] = await flightTickets.getAirlineById.call(aId);
@@ -200,6 +225,7 @@ contract('FlightTickets', accounts => {
     assert.equal(tNewQuantity, tQuantity - 1);
   });
 
+  // Check that a two-tickets flight is bought correctly and both airlines receive their payments
   it('books a one-stop flight', async () => {
     await flightTickets.addAirline('Third Airline', accounts[5], aLogoHash, { from: accounts[0] });
     aIdNew = await flightTickets.aIdLast.call();
@@ -228,6 +254,7 @@ contract('FlightTickets', accounts => {
     assert.equal(tNewQuantity2, tQuantity2 - 1);
   });
 
+  // Check that emergency stop works
   it('pauses the contract', async () => {
     await flightTickets.pause();
     assert.ok(await hasReverted(
@@ -235,12 +262,14 @@ contract('FlightTickets', accounts => {
     ));
   });
 
+  // Check that the stopped contract can be reenabled again
   it('unpauses the contract', async () => {
     await flightTickets.unpause();
     await flightTickets.addAirline('New Test Airline', accounts[2], aLogoHash, { from: accounts[0] });
     assert.ok(await flightTickets.airlineExists('New Test Airline'));
   });
 
+  // Check that the contract can be destroyed
   it('kills the contract', async () => {
     await flightTickets.destroy();
     try {
--- test/TestFlightTicketsRegistry.js
+++ test/TestFlightTicketsRegistry.js
@@ -1,6 +1,7 @@
 const FlightTickets = artifacts.require("FlightTickets");
 const FlightTicketsRegistry = artifacts.require("FlightTicketsRegistry");
 
+// Function to verify that a contract call has failed (reverted) during execution
 async function hasReverted(contractCall) {
   try {
     await contractCall;
@@ -17,26 +18,36 @@ contract('FlightTicketsRegistry', accounts => {
 
   let registry, flightTickets, flightTicketsNew;
 
+  // Deploy the contracts
+  before(async () => {
+    flightTickets = await FlightTickets.new();
+    registry = await FlightTicketsRegistry.new(flightTickets.address);
+  });
+
+  // Check that contract ownership is set properly
   it('sets the owner', async () => {
-    registry = await FlightTicketsRegistry.deployed();
     assert.equal(await registry.owner.call(), owner);
   });
 
+  // Check that the main contract address is set correctly
   it('sets the backend contract address', async () => {
-    flightTickets = await FlightTickets.deployed();
     assert.equal(await registry.backendContract.call(), flightTickets.address);
   });
 
+  // Upgrade to new version: create a new main contract and change it
+  // in the registry (that's the main purpose of the registry)
   it('upgrades the registry to the new backend contract', async () => {
     flightTicketsNew = await FlightTickets.new();
     await registry.changeBackend(flightTicketsNew.address, { from: owner });
     assert.equal(await registry.backendContract.call(), flightTicketsNew.address);
   });
 
+  // Check that the previous main contract address is saved for reference
   it('saves the previous backend contract address', async () => {
     assert.equal(await registry.previousBackends.call(0), flightTickets.address);
   });
 
+  // Check that only the owner can do such an upgrade
   it('does not allow a non-owner to upgrade the registry', async () => {
     let flightTicketsV3 = await FlightTickets.new();
     assert.ok(await hasReverted(
